a2f807003c35934d1f7586f9401d13e36f2a4a1a
cilledyr <47023263+cilledyr@users.noreply.github.com>
2020-05-11 09:35:51 +0200

    Updated AddNewTaskAndSetItRunning

--- src/ficety/bll/BllManager.java
+++ src/ficety/bll/BllManager.java
@@ -147,12 +147,10 @@ public class BllManager implements IBLL {
             tempSession = dalManager.addFinishTimeToSession(lu.getCurrentSession(), LocalDateTime.now());
             lu.setCurrentSession(null);          
         }
-        else
-        {
-            tempSession =  startStopSession();
-        }
+        
         Task currentTask = addNewTaskToDB(taskName, associatedProject);
         lu.setCurrentTask(currentTask);
+        tempSession =  startStopSession();
         //Session curSession = startStopSession();
         Pair<Task, Session> temp = new Pair(currentTask, tempSession);
         return temp;
