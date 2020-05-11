f64d418dd68d4a7c9314746cdf0290068a75009c
filipus30 <56824843+filipus30@users.noreply.github.com>
2020-05-11 19:51:10 +0200

    .
    .

--- src/ficety/gui/controller/AdminViewController.java
+++ src/ficety/gui/controller/AdminViewController.java
@@ -482,7 +482,22 @@ public class AdminViewController extends JFrame implements Initializable {
 
     @FXML
     private void handle_startStop(ActionEvent event) {
-        //datasession.add(UVM.startStopSession());
+      if(added)
+        {
+        datasession.add(UVM.startStopSession());
+        added = false;
+        }
+        else{
+        UVM.startStopSession();
+        added = true;
+        }
+
+        if(isTimerRunning){
+            timer.stop();
+        isTimerRunning = false;}
+        else{
+        timer.start();
+        isTimerRunning = true;}
           }
 
 AnimationTimer timer = new AnimationTimer() {
